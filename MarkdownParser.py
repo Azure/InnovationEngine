3000dae3fbd463ff63f68e02d7770df9343aece6
jasonmesser7 <91570951+jasonmesser7@users.noreply.github.com>
2022-12-20 12:01:27 -0800

    Parser and executor code (#1)
    * Created main.py and am doing a test commit
    
    * test commit
    
    * test commit
    
    * Parses a markdown file and pulls out headings, code blocks, and paragraphs
    
    * took away a few magic values & cleaned up code a bit
    
    * added the initial executor code complete with repl behavior, by no means complete and it will have bugs but it would be good to get some eyes on it before I go too much further
    
    * added a few more comments and deleted a few unused functions
    
    * Added testing directions of using python3 main.py to test
    
    * Added automated testing functionality with fuzzy matching
    
    * cleaned up code a bit and added test functionality
    
    * testing
    
    * Added github actions for automated testing
    
    * Fixing github actions
    
    * bug fix for actions
    
    * Added push to main as a trigger
    
    * bug fix
    
    * testing
    
    * changed file type to yml
    
    * testing
    
    * fixed bug with fuzzywuzzy
    
    * changing sensitivity so that actions should fail as there is a warning
    
    * Changing Sensitivity and adjust actions test
    
    * testing
    
    * test should pass now
    
    * Trying to enable manual workflow trigger
    
    * testing github actions
    
    * Changing default test
    
    * added the ability to set command line variables from both comments and .ini files which are key value pairs
    
    * updated README.md and added new test scripts
    
    * fixed test file and changed github action to point to test file not readme
    
    * Hardedned program to deal with broken markdown files, there is still no good way to deal with interactive bash commands though
    
    * added some exception handling so the program doesn't crash on a timeout
    
    * Changed Parser file class to MarkdownParser to avoid name clash with std parser, added test scripts folder for testing, and added some additional error handling for commands
    
    * removing exit(1) from test section as it terminates early if multiple files are being tested
    
    * removed exit(1) from main when file does not parse correctly
    
    * removed exit 1 to stop early failures with multiple files
    
    * adding random string to end of resource group in testing section so that there are not name conflicts. Catch is the document itself has to use MY_RESOURCE_GROUP_NAME as the variable for testing
    
    * cleaned up file structure and fixed a few bugs in Readme.md
    
    * added verion locks to requirements.txt, deleted hardcoded vm create document, added .vscode to .gitignore, and modified test script to work based on new location

---
+++ MarkdownParser.py
@@ -0,0 +1,236 @@
+#Given a markdown file, this should break out the headings, paragraphs, executable commands etc. 
+import re
+
+class MarkdownParser:
+  
+
+    def __init__(self, markdownFilepath):
+        self.markdownFilepath = markdownFilepath
+        self.markdownElements = []
+        self.codeBlockType = '```'
+        self.headingType = '#'
+        self.paragraphType = 'p'
+        self.commentType = '<!--'
+
+       
+    def parseMarkdown(self):
+        self.markdownFile = open(self.markdownFilepath)
+        special_characters = ["#", "`", "~" ,"-" ]
+        
+        char = self.markdownFile.read(1)
+
+        while char:
+
+            if char == '#':
+                self.processHeading(char)
+                
+            elif char == '`':
+                if self.checkForCodeBlock(char):
+                    subtype, command = self.processCodeSample(char)
+                    self.createAndAppendElement(self.codeBlockType, subtype, command)
+
+            elif char == '<':
+                if self.checkForComment():
+                    self.processComment(char)
+                    
+            # No specific tag, creating paragraphs and continuing to loop to a special char
+            
+            else:
+                self.processParagraph(char)
+                
+            char = self.markdownFile.read(1)
+
+        self.markdownFile.close()
+
+    # Iterates through line adding each character to heading string. Also collects the heading type.
+    # Creates a markdown element storing the subtype and text value. Appends to all markdown elements.
+    def processHeading(self, char):
+        text = ""
+        headingCount = 0
+        while char != '\n':
+            if char == '#':
+                headingCount += 1
+           
+            text += char
+            char = self.markdownFile.read(1)
+        
+        text += char # Adds the new line character to the heading
+        subtype = "h" + str(headingCount)
+        self.createAndAppendElement(self.headingType, subtype, text)
+
+    # Iterates through text until 3 back ticks ``` are found signifying the end of code block
+    def processCodeSample(self, char):
+        command = ""
+        subtype = ""
+        endOfCodeBlock = False
+        self.markdownFile.read(2)
+        # Reading through initial backtick line to see what the subtype is
+        while char != '\n':
+            char = self.markdownFile.read(1)
+            subtype += char
+
+        while not endOfCodeBlock:
+            if (char == '`'):
+                if self.checkForCodeBlock(char):
+                    endOfCodeBlock = True
+                    # Read the remaining bash ticks
+                    self.markdownFile.read(2)          
+
+            else:
+                command += char
+                char = self.markdownFile.read(1)
+
+        # Should we read a new line here?
+        subtype = subtype.strip()
+        command = command.strip()
+        return subtype, command
+        
+     
+    # Iterates until we find a heading or back-tick. If a heading is found it creates paragraph
+    # and leaves the function. If a code block or comment is found, it creates a paragraph
+    # then rewinds a character and returns to the main loop to process the comment or code block
+    def processParagraph(self, char):
+        paragraph = ""
+        while char != '#' and char != '':
+            if char == '`':
+                if self.checkForCodeBlock(char):
+                    self.createAndAppendElement(self.paragraphType, 'paragraph', paragraph.strip())
+                    self.markdownFile.seek(currentPosition)
+                    return
+                   # self.processCodeSample(char)
+                else:
+                    paragraph += char           
+            elif char == '<':
+                if self.checkForComment:
+                    self.createAndAppendElement(self.paragraphType, 'paragraph', paragraph.strip())
+                    self.markdownFile.seek(currentPosition)
+                    return
+                    #self.processComment(char)
+                else:
+                    paragraph += char
+            else: 
+                paragraph += char
+            
+            currentPosition = self.markdownFile.tell()
+            char = self.markdownFile.read(1)
+            
+        if len(self.markdownElements) != 0 and "prerequisites" in self.markdownElements[-1][1].value.lower():
+            self.createAndAppendElement(self.paragraphType, 'prerequisites', paragraph.strip())
+
+        elif len(self.markdownElements) != 0 and "next steps" in self.markdownElements[-1][1].value.lower():
+            self.createAndAppendElement(self.paragraphType, 'next steps', paragraph.strip())
+
+        else:
+            self.createAndAppendElement(self.paragraphType, 'paragraph', paragraph.strip())
+
+        self.markdownFile.seek(currentPosition)
+
+    def processComment(self, char):
+        endOfComment = False
+        comment = "<"
+     
+        while not endOfComment and char != '':
+            currentPosition = self.markdownFile.tell()
+            if self.markdownFile.read(1) == '-' and self.markdownFile.read(1) == '-' and self.markdownFile.read(1) == '>':
+                comment += char + '-->'
+                endOfComment = True
+                continue
+            elif self.markdownFile.read(1) == '`':
+                if self.checkForCodeBlock(char):
+                    subtype, command = self.processCodeSample(char)
+                    self.createAndAppendElement(self.commentType, subtype, command)
+                    comment += command
+            else:
+                self.markdownFile.seek(currentPosition)
+                comment += char
+
+            char = self.markdownFile.read(1)
+
+        if "expected_similarity" in comment:
+            results,subtype = self.processResultsBlock()
+            similarity = re.findall(r"\d*\.?\d+", comment)[0]
+            # outputblock = "```output\n" + results + "\n```"
+            # self.createAndAppendElement(self.paragraphType, 'paragraph', outputblock.strip())
+            # Loops through elements starting at the end looking for the most recent
+            # Codeblock markdown item and adds the results 
+            for i, markdownElement in reversed(list(enumerate(self.markdownElements))):
+                if markdownElement[0] == self.codeBlockType:
+                    self.markdownElements[i][1].results = results
+                    self.markdownElements[i][1].similarity = similarity
+            
+                    break
+            self.createAndAppendElement(self.codeBlockType, subtype, results)
+        else:
+            self.createAndAppendElement(self.commentType, None, comment)
+
+   
+
+
+    def createAndAppendElement(self, type, subtype, value):
+        element = MarkdownElement(subtype, value)
+        self.markdownElements.append((type, element))
+
+    # Helper function ran when we hit a backtick. It checks the next two characters to see
+    # if they are also backticks. If so, we enter a code block and return true
+    def checkForCodeBlock(self, char):
+        currentPosition = self.markdownFile.tell()
+        if self.markdownFile.read(1) == '`' and self.markdownFile.read(1) == '`':
+            self.markdownFile.seek(currentPosition)
+            return True
+        else:
+            self.markdownFile.seek(currentPosition)
+            return False
+    
+    def checkForComment(self):
+        currentPosition = self.markdownFile.tell()
+        if self.markdownFile.read(1) == '!' and self.markdownFile.read(1) == '-' and self.markdownFile.read(1) == '-':
+            self.markdownFile.seek(currentPosition)
+            return True
+        else:
+            self.markdownFile.seek(currentPosition)
+            return False
+
+    def processResultsBlock(self):
+        results = ""
+        subtype = ""
+        endOfCodeBlock = False
+
+        char = self.markdownFile.read(1)
+        
+        while char != '`':
+            char = self.markdownFile.read(1)
+        
+        self.markdownFile.read(2)
+        while char != '\n':
+            char = self.markdownFile.read(1)
+            subtype += char
+
+        while not endOfCodeBlock:
+            if (char == '`'):
+                if self.checkForCodeBlock(char):
+                    endOfCodeBlock = True
+                    # Read the remaining bash ticks
+                    self.markdownFile.read(2)
+                
+            else:
+                results += char
+            # Read all 3 back ticks
+            char = self.markdownFile.read(1)
+        
+        return results.strip(), subtype.strip()
+    # If we want to add process for bold and italicized text
+    def processBoldText(self,char):
+        pass
+
+    # If want to process dashes for hidden titles etc. 
+    def processDash(self, char):
+        pass
+
+class MarkdownElement:
+
+    def __init__(self, subtype, value):
+        self.subtype = subtype
+        self.value = value
+        self.results = None
+        self.similarity = 1.0
+
