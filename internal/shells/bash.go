4fa30cde6a276b6b3b887da22f200226ceba98ca
vmarcella <vmarcella@microsoft.com>
2023-04-25 21:13:11 -0700

    [update] reference.

--- internal/shells/bash.go
+++ internal/shells/bash.go
@@ -8,17 +8,17 @@ import (
 
 // Executes a bash command and returns the output or error.
 func ExecuteBashCommand(command string, env map[string]string, inherit_environment_variables bool) (string, error) {
-	command_to_execute := exec.Command("bash", "-c", command)
+	commandToExecute := exec.Command("bash", "-c", command)
 
 	if inherit_environment_variables {
-		command_to_execute.Env = os.Environ()
+		commandToExecute.Env = os.Environ()
 	}
 
 	for k, v := range env {
-		command_to_execute.Env = append(command_to_execute.Env, fmt.Sprintf("%s=%s", k, v))
+		commandToExecute.Env = append(commandToExecute.Env, fmt.Sprintf("%s=%s", k, v))
 	}
 
-	out, err := command_to_execute.Output()
+	out, err := commandToExecute.Output()
 	if err != nil {
 		return "", fmt.Errorf("error executing bash command: %w", err)
 	}
