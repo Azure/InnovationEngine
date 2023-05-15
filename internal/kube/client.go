e8ad9abec74a2907e78c70d2a68eecac826cbe4b
vmarcella <vmarcella@microsoft.com>
2023-05-15 12:03:49 -0700

    [add] kubernetes client & code to grab the clientset.

---
+++ internal/kube/client.go
@@ -0,0 +1,35 @@
+package kube
+
+import (
+	"os"
+	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func GetKubernetesClient() (*kubernetes.Clientset, error) {
+	var config *rest.Config
+	var err error
+
+	if _, err := rest.InClusterConfig(); err != nil {
+		kubeConfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
+		config, err = clientcmd.BuildConfigFromFlags("", kubeConfig)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return clientset, nil
+}
