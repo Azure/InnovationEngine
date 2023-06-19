886c54c9d20fb24a972dc052ff1302fd6fc70771
vmarcella <vmarcella@microsoft.com>
2023-06-19 12:34:54 -0700

    [update] logging to store the low level output into a file, update log statements across the codebase.

--- cmd/ie/commands/root.go
+++ cmd/ie/commands/root.go
@@ -4,16 +4,28 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/Azure/InnovationEngine/internal/logging"
 	"github.com/spf13/cobra"
 )
 
+// The root command for the CLI. Currently initializes the logging for all other
+// commands.
 var rootCommand = &cobra.Command{
 	Use:   "ie",
 	Short: "The innovation engine.",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		logLevel, err := cmd.Flags().GetString("log-level")
+		if err != nil {
+			panic(err)
+		}
+		logging.Init(logging.LevelFromString(logLevel))
+	},
 }
 
 // Entrypoint into the Innovation Engine CLI.
 func ExecuteCLI() {
+	rootCommand.PersistentFlags().String("log-level", string(logging.Error), "Configure the log level")
+
 	if err := rootCommand.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
