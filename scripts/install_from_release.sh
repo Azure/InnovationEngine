8e75ac5f6194061cf46cd730b6a0e0a3a5603166
vmarcella <vmarcella@microsoft.com>
2023-07-18 12:09:42 -0700

    [update] installation script.

--- scripts/install_from_release.sh
+++ scripts/install_from_release.sh
@@ -2,11 +2,16 @@ set -e
 
 # Download the binary from the latest
 wget -O ie https://github.com/Azure/InnovationEngine/releases/download/latest/ie
+wget -O scenarios.zip https://github.com/Azure/InnovationEngine/releases/download/latest/scenarios.zip
 
 # Setup permissions & move to the local bin
 chmod +x ie
 mkdir -p ~/.local/bin
 mv ie ~/.local/bin
+
+# Unzip the scenarios
+unzip scenarios.zip -d ~
+rm scenarios.zip
 
 # Export the path to IE if it's not already available
 if [[ !"$PATH" =~ "~/.local/bin" || !"$PATH" =~ "$HOME/.local/bin" ]]; then
