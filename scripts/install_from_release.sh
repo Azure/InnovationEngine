6b4742a85448bbe478e691a82b68c7a95302edfe
vmarcella <vmarcella@microsoft.com>
2023-07-18 12:54:52 -0700

    [update] installation script to be more silent.

--- scripts/install_from_release.sh
+++ scripts/install_from_release.sh
@@ -1,19 +1,22 @@
 set -e
 
 # Download the binary from the latest
-wget -O ie https://github.com/Azure/InnovationEngine/releases/download/latest/ie
-wget -O scenarios.zip https://github.com/Azure/InnovationEngine/releases/download/latest/scenarios.zip
+echo "Installing IE & scenarios from the latest release..."
+wget -q -O ie https://github.com/Azure/InnovationEngine/releases/download/latest/ie > /dev/null
+wget -q -O scenarios.zip https://github.com/Azure/InnovationEngine/releases/download/latest/scenarios.zip > /dev/null
 
 # Setup permissions & move to the local bin
-chmod +x ie
-mkdir -p ~/.local/bin
-mv ie ~/.local/bin
+chmod +x ie > /dev/null
+mkdir -p ~/.local/bin > /dev/null
+mv ie ~/.local/bin > /dev/null
 
-# Unzip the scenarios
-unzip scenarios.zip -d ~
-rm scenarios.zip
+# Unzip the scenarios, overwrite if they already exist.
+unzip -o scenarios.zip -d ~ > /dev/null
+rm scenarios.zip > /dev/null
 
 # Export the path to IE if it's not already available
 if [[ !"$PATH" =~ "~/.local/bin" || !"$PATH" =~ "$HOME/.local/bin" ]]; then
   export PATH="$PATH:~/.local/bin"
 fi
+
+echo "Done."
