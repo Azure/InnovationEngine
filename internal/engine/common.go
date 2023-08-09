d3dc493d03e174bb566e064f8a6f3032dbf0546f
vmarcella <vmarcella@microsoft.com>
2023-08-09 16:37:18 -0700

    [add] styling for the ocd status to set it's foreground to black.

--- internal/engine/common.go
+++ internal/engine/common.go
@@ -8,13 +8,14 @@ import (
 
 // Styles used for rendering output to the terminal.
 var (
-	scenarioTitleStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#6CB6FF")).Align(lipgloss.Center).Bold(true).Underline(true)
-	stepTitleStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("#518BAD")).Align(lipgloss.Left).Bold(true)
-	spinnerStyle       = lipgloss.NewStyle().Foreground(lipgloss.Color("#518BAD"))
-	verboseStyle       = lipgloss.NewStyle().Foreground(lipgloss.Color("#437684")).Align(lipgloss.Left)
-	checkStyle         = lipgloss.NewStyle().Foreground(lipgloss.Color("#32CD32"))
-	errorStyle         = lipgloss.NewStyle().Foreground(lipgloss.Color("#FF0000"))
-	errorMessageStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("#FF5733"))
+	scenarioTitleStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("#6CB6FF")).Align(lipgloss.Center).Bold(true).Underline(true)
+	stepTitleStyle       = lipgloss.NewStyle().Foreground(lipgloss.Color("#518BAD")).Align(lipgloss.Left).Bold(true)
+	spinnerStyle         = lipgloss.NewStyle().Foreground(lipgloss.Color("#518BAD"))
+	verboseStyle         = lipgloss.NewStyle().Foreground(lipgloss.Color("#437684")).Align(lipgloss.Left)
+	checkStyle           = lipgloss.NewStyle().Foreground(lipgloss.Color("#32CD32"))
+	errorStyle           = lipgloss.NewStyle().Foreground(lipgloss.Color("#FF0000"))
+	errorMessageStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("#FF5733"))
+	ocdStatusUpdateStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#000000"))
 )
 
 // Indents a multi-line command to be nested under the first line of the
