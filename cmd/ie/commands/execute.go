24dbf9d287b4ebff0206c74fcbb4110314eee3ca
vmarcella <vmarcella@microsoft.com>
2023-09-11 18:13:56 +0000

    [add] --variables flag to allow for environment variables to be overidden via the CLI.

--- cmd/ie/commands/execute.go
+++ cmd/ie/commands/execute.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/Azure/InnovationEngine/internal/engine"
 	"github.com/Azure/InnovationEngine/internal/logging"
@@ -21,6 +22,7 @@ func init() {
 	executeCommand.PersistentFlags().String("correlation-id", "", "Adds a correlation ID to the user agent used by a scenarios azure-cli commands.")
 	executeCommand.PersistentFlags().String("subscription", "", "Sets the subscription ID used by a scenarios azure-cli commands. Will rely on the default subscription if not set.")
 	executeCommand.PersistentFlags().String("working-directory", ".", "Sets the working directory for innovation engine to operate out of. Restores the current working directory when finished.")
+	executeCommand.PersistentFlags().StringArray("variables", []string{}, "Sets the environment variable for the scenario. Format: --variables <key>=<value>")
 }
 
 var executeCommand = &cobra.Command{
@@ -30,6 +32,7 @@ var executeCommand = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		markdownFile := args[0]
 		if markdownFile == "" {
+			logging.GlobalLogger.Errorf("Error: No markdown file specified.")
 			cmd.Help()
 			os.Exit(1)
 		}
@@ -39,7 +42,22 @@ var executeCommand = &cobra.Command{
 		subscription, _ := cmd.Flags().GetString("subscription")
 		correlationId, _ := cmd.Flags().GetString("correlation-id")
 		environment, _ := cmd.Flags().GetString("environment")
+		environmentVariables, _ := cmd.Flags().GetStringArray("variables")
 		workingDirectory, _ := cmd.Flags().GetString("working-directory")
+
+		userDefinedEnvironmentVariables := make(map[string]string)
+
+		for _, environmentVariable := range environmentVariables {
+			keyValuePair := strings.SplitN(environmentVariable, "=", 2)
+			if len(keyValuePair) != 2 {
+				logging.GlobalLogger.Errorf("Error: Invalid environment variable format: %s", environmentVariable)
+				fmt.Printf("Error: Invalid environment variable format: %s", environmentVariable)
+				cmd.Help()
+				os.Exit(1)
+			}
+
+			userDefinedEnvironmentVariables[keyValuePair[0]] = keyValuePair[1]
+		}
 
 		innovationEngine := engine.NewEngine(engine.EngineConfiguration{
 			Verbose:          verbose,
