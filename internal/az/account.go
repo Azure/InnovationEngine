90ed1c7c98cb2f321b3be10465977cd166bd6378
vmarcella <vmarcella@microsoft.com>
2023-09-29 15:40:09 -0700

    [remove] unused login.

--- internal/az/account.go
+++ internal/az/account.go
@@ -7,24 +7,6 @@ import (
 	"github.com/Azure/InnovationEngine/internal/shells"
 )
 
-func LoginWithMSI() error {
-	// Login
-	command := "az login --identity"
-	logging.GlobalLogger.Info("Logging into the azure cli.")
-	output, err := shells.ExecuteBashCommand(command, shells.BashCommandConfiguration{EnvironmentVariables: map[string]string{}, InteractiveCommand: false, WriteToHistory: false, InheritEnvironment: true})
-
-	logging.GlobalLogger.Debugf("Login stdout: %s", output.StdOut)
-	logging.GlobalLogger.Debugf("Login stderr: %s", output.StdErr)
-
-	if err != nil {
-		logging.GlobalLogger.Errorf("Failed to login %s", err)
-		return err
-	}
-
-	logging.GlobalLogger.Info("Login successful.")
-	return nil
-}
-
 func SetSubscription(subscription string) error {
 	if subscription != "" {
 		command := fmt.Sprintf("az account set --subscription %s", subscription)
