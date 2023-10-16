8dfabc48e7932f55aaf7ea64ea5a4ce3f08ba098
Rahul Gupta <guptar@microsoft.com>
2023-10-16 15:10:34 -0700

    [refactor] command rendering

--- internal/engine/execution_test.go
+++ internal/engine/execution_test.go
@@ -1,1 +1,23 @@
 package engine
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExecuteBlock(t *testing.T) {
+	blocks := []string{
+		"echo \"hello \\\nworld\"", // tutorial.md
+		"echo hello \\\nworld",
+		"echo \"hello world\"",
+		"echo hello world",
+	}
+	for _, blockCommand := range blocks {
+		t.Run("render command", func(t *testing.T) {
+			_, err := renderCommand(blockCommand)
+			assert.Equal(t, nil, err)
+		})
+	}
+
+}
