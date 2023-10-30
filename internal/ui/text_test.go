c08329c86c99ec754514b13e967ca61e1f929513
rguptar <69279773+rguptar@users.noreply.github.com>
2023-10-30 10:08:30 -0700

    [fix] remove horizontal align (#121)
    * [fix] remove horizontal align
    
    * [update] command output to not be tabbed

---
+++ internal/ui/text_test.go
@@ -0,0 +1,17 @@
+package ui
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVerboseStyle(t *testing.T) {
+	text := `aaaa
+	b`
+	styledText := VerboseStyle.Render(text)
+	expectedStyledText := `aaaa
+	b   `
+	assert.Equal(t, expectedStyledText, styledText)
+	assert.Equal(t, text, RemoveHorizontalAlign(styledText))
+}
