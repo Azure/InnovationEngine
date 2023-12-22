a38bb66edbfa492841f78223ba8af314ff91e5fc
Mitchell Bifeld <mbifeld@gmail.com>
2023-12-22 15:34:04 -0800

    Update install_from_release.sh to account for localized files

--- scripts/install_from_release.sh
+++ scripts/install_from_release.sh
@@ -1,9 +1,19 @@
 set -e
 
+# Define the language parameter (default is 'en')
+lang=${1:-'en'}
+
+# Map the language parameter to the corresponding scenarios file
+if [ "$lang" = "en" ]; then
+  scenarios='scenarios.zip'
+else
+  scenarios="$lang-scenarios.zip"
+fi
+
 # Download the binary from the latest
 echo "Installing IE & scenarios from the latest release..."
 wget -q -O ie https://github.com/Azure/InnovationEngine/releases/download/latest/ie > /dev/null
-wget -q -O scenarios.zip https://github.com/Azure/InnovationEngine/releases/download/latest/scenarios.zip > /dev/null
+wget -q -O scenarios.zip https://github.com/Azure/InnovationEngine/releases/download/latest/$scenarios > /dev/null
 
 # Setup permissions & move to the local bin
 chmod +x ie > /dev/null
