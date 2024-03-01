1216639bf157485742bd983dc8ed89cad66e7490
Vincenzo Marcella <6026326+vmarcella@users.noreply.github.com>
2024-03-01 15:57:39 -0800

    Update the bash command execution to specify `set -e` when executing. (#155)
    This PR allows for the innovation engine to correctly terminate a code
    block after any command inside of it has failed. Previously the
    innovation engine would rely on the last command in the code block being
    executed to check for & report failure which meant that code blocks
    which executed multiple commands would not fail if any command other
    than the last one had an error.

--- internal/shells/bash_test.go
+++ internal/shells/bash_test.go
@@ -53,9 +53,13 @@ func TestEnvironmentVariableValidationAndFiltering(t *testing.T) {
 			"_AnotherValidKey": "value2",
 		}
 
-    if len(validEnvMap) != len(expectedValidEnvMap) {
-      t.Errorf("Expected validEnvMap to have %d keys, got %d", len(expectedValidEnvMap), len(validEnvMap))
-    }
+		if len(validEnvMap) != len(expectedValidEnvMap) {
+			t.Errorf(
+				"Expected validEnvMap to have %d keys, got %d",
+				len(expectedValidEnvMap),
+				len(validEnvMap),
+			)
+		}
 
 		for key, value := range validEnvMap {
 			if expectedValue, ok := expectedValidEnvMap[key]; !ok || value != expectedValue {
@@ -63,4 +67,111 @@ func TestEnvironmentVariableValidationAndFiltering(t *testing.T) {
 			}
 		}
 	})
+}
+
+func TestBashCommandExecution(t *testing.T) {
+	// Ensures that if a command succeeds, the output is returned.
+	t.Run("Valid command execution", func(t *testing.T) {
+		cmd := "printf hello"
+		result, err := ExecuteBashCommand(
+			cmd,
+			BashCommandConfiguration{
+				EnvironmentVariables: nil,
+				InheritEnvironment:   true,
+				InteractiveCommand:   false,
+				WriteToHistory:       false,
+			},
+		)
+		if err != nil {
+			t.Errorf("Expected err to be nil, got %v", err)
+		}
+		if result.StdOut != "hello" {
+			t.Errorf("Expected result to be non-empty, got '%s'", result.StdOut)
+		}
+	})
+
+	// Ensures that if a command fails, an error is returned.
+	t.Run("Invalid command execution", func(t *testing.T) {
+		cmd := "not_real_command"
+		_, err := ExecuteBashCommand(
+			cmd,
+			BashCommandConfiguration{
+				EnvironmentVariables: nil,
+				InheritEnvironment:   true,
+				InteractiveCommand:   false,
+				WriteToHistory:       false,
+			},
+		)
+
+		if err == nil {
+			t.Errorf("Expected an error to occur, but the command succeeded.")
+		}
+
+	})
+
+	// Test the execution of commands with multiple subcommands.
+	t.Run("Command with multiple subcommands", func(t *testing.T) {
+		cmd := "printf hello; printf world"
+		result, err := ExecuteBashCommand(
+			cmd,
+			BashCommandConfiguration{
+				EnvironmentVariables: nil,
+				InheritEnvironment:   true,
+				InteractiveCommand:   false,
+				WriteToHistory:       false,
+			},
+		)
+		if err != nil {
+			t.Errorf("Expected err to be nil, got %v", err)
+		}
+		if result.StdOut != "helloworld" {
+			t.Errorf("Expected result to be non-empty, got '%s'", result.StdOut)
+		}
+	})
+
+	// Ensures that if one of the subcommands fail, the other commands do
+	// as well.
+	t.Run("Command with multiple subcommands exits on first error", func(t *testing.T) {
+		cmd := "printf hello; not_real_command; printf world"
+		_, err := ExecuteBashCommand(
+			cmd,
+			BashCommandConfiguration{
+				EnvironmentVariables: nil,
+				InheritEnvironment:   true,
+				InteractiveCommand:   false,
+				WriteToHistory:       false,
+			},
+		)
+
+		if err == nil {
+			t.Errorf("Expected an error to occur, but the command succeeded.")
+		}
+
+	})
+
+	// Ensures that commands can access environment variables passed into
+	// the configuration.
+	t.Run("Command with environment variables", func(t *testing.T) {
+		cmd := "printf $TEST_ENV_VAR"
+		result, err := ExecuteBashCommand(
+			cmd,
+			BashCommandConfiguration{
+				EnvironmentVariables: map[string]string{
+					"TEST_ENV_VAR": "hello",
+				},
+				InheritEnvironment: true,
+				InteractiveCommand: false,
+				WriteToHistory:     false,
+			},
+		)
+
+		if err != nil {
+			t.Errorf("Expected err to be nil, got %v", err)
+		}
+
+		if result.StdOut != "hello" {
+			t.Errorf("Expected result to be non-empty, got '%s'", result.StdOut)
+		}
+	})
+
 }
