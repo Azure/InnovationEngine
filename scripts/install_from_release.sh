09da91d5e330f9f5cbb4146f96268d0cd514d624
Adrian Joian <ajoian@microsoft.com>
2024-03-04 14:02:56 +0100

    Adding review comments - L14 setting a default value for SCENARIOS - L21 variable quotation - L21-L53 reworking the logic to choose translations files based on locale passed to the script

--- scripts/install_from_release.sh
+++ scripts/install_from_release.sh
@@ -10,44 +10,46 @@ if [ "$RELEASE" == "" ]; then
   RELEASE="latest"
 fi
 
+# Set a default scenarios file
+SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/scenarios.zip"
+
 # Map the language parameter to the corresponding scenarios file
 # If no parameter, download the scenarios from IE
 MAIN_LANG_PREFIX="$(echo "$LANG" | head -c2 | tr '[:upper:]' '[:lower:]')"
-LANG_ARRAY_1=("de" "es" "fr" "it" "nl" "pt")
-LANG_ARRAY_2=("cs" "hu" "id" "ja" "ko" "pl" "pt" "ru" "sv" "tr")
+LANG_ARRAY=("de" "es" "fr" "it" "nl" "pt" "zh" "cs" "hu" "id" "ja" "ko" "pl" "pt" "ru" "sv" "tr")
 
-if [ "$MAIN_LANG_PREFIX" = "" ] || [ "$MAIN_LANG_PREFIX" = "en" ]; then
-  SCENARIOS="https://github.com/Azure/InnovationEngine/releases/download/$RELEASE/scenarios.zip"
-elif [ "$MAIN_LANG_PREFIX" = "pt" ]; then
-  if [ "$LANG" = 'pt-pt' ]; then
-    SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/pt-pt-scenarios.zip"
-  elif [ "$LANG" = 'pt-br' ]; then
-    SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/pt-br-scenarios.zip"
-  fi
-elif [ "$MAIN_LANG_PREFIX" = "zh" ]; then
-  if [ "$LANG" = "zh-cn" ]; then
-    SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/zh-cn-scenarios.zip"
-  elif [ "$LANG" = "zh-tw" ]; then 
-    SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/zh-tw-scenarios.zip"
-  else
-    SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/zh-cn-scenarios.zip"
-  fi
-elif [[ ${LANG_ARRAY_1[@]} =~ $MAIN_LANG_PREFIX ]]; then
-  # for any other language that we do not have a specific scenarios file for, we will use the generic one
+if [[ "${LANG_ARRAY[*]}" =~ "$MAIN_LANG_PREFIX" ]]; then
   SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/$MAIN_LANG_PREFIX-$MAIN_LANG_PREFIX-scenarios.zip"
-elif [[ ${LANG_ARRAY_2[@]} =~ $MAIN_LANG_PREFIX ]]; then
-  SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/$MAIN_LANG_PREFIX-$MAIN_LANG_PREFIX-scenarios.zip"
+  if [ "$MAIN_LANG_PREFIX" = "pt" ]; then
+    if [ "$LANG" = "pt-pt" ]; then
+      SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/pt-pt-scenarios.zip"
+    elif [ "$LANG" = "pt-br" ]; then
+      SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/pt-br-scenarios.zip"
+    else
+      SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/pt-pt-scenarios.zip"
+    fi
+  fi
+  if [ "$MAIN_LANG_PREFIX" = "zh" ]; then
+    if [ "$LANG" = "zh-cn" ]; then
+      SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/zh-cn-scenarios.zip"
+    elif [ "$LANG" = "zh-tw" ]; then 
+      SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/zh-tw-scenarios.zip"
+    else
+      SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/zh-cn-scenarios.zip"
+    fi
+  fi
   if [ "$MAIN_LANG_PREFIX" = "cs" ]; then
     SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/$MAIN_LANG_PREFIX-cz-scenarios.zip"
-  elif [ "$MAIN_LANG_PREFIX" = "ja" ]; then
+  fi
+  if [ "$MAIN_LANG_PREFIX" = "ja" ]; then
     SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/$MAIN_LANG_PREFIX-jp-scenarios.zip"
-  elif [ "$MAIN_LANG_PREFIX" = "ko" ]; then
+  fi
+  if [ "$MAIN_LANG_PREFIX" = "ko" ]; then
     SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/$MAIN_LANG_PREFIX-kr-scenarios.zip"
-  elif [ "$MAIN_LANG_PREFIX" = "sv" ]; then
+  fi
+  if [ "$MAIN_LANG_PREFIX" = "sv" ]; then
     SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/$MAIN_LANG_PREFIX-se-scenarios.zip"
   fi
-else
-  SCENARIOS="https://github.com/Azure/InnovationEngine/releases/download/$RELEASE/scenarios.zip"
 fi
 
 # Download the binary
