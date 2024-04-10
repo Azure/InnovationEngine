f4b17e6f4ca68b3aa80761a567899f354e39467d
vmarcella <vmarcella@microsoft.com>
2024-04-10 07:24:07 -0700

    [update] versions for locale fallbacks.

--- scripts/install_docs_from_release.sh
+++ scripts/install_docs_from_release.sh
@@ -19,39 +19,40 @@ if [ "$LANG" != "" ]; then
 	# Map the language parameter to the corresponding scenarios file
 	# If no parameter, download the scenarios from IE
 	MAIN_LANG_PREFIX="$(echo "$LANG" | head -c2 | tr '[:upper:]' '[:lower:]')"
+  echo $MAIN_LANG_PREFIX
 	LANG_ARRAY=("de" "es" "fr" "it" "nl" "pt" "zh" "cs" "hu" "id" "ja" "ko" "pl" "pt" "ru" "sv" "tr")
 
 	if [[ "${LANG_ARRAY[*]}" =~ "$MAIN_LANG_PREFIX" ]]; then
-		SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/$MAIN_LANG_PREFIX-$MAIN_LANG_PREFIX-scenarios.zip"
+		SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/$RELEASE/$MAIN_LANG_PREFIX-$MAIN_LANG_PREFIX-scenarios.zip"
 		if [ "$MAIN_LANG_PREFIX" = "pt" ]; then
 			if [ "$LANG" = "pt-pt" ]; then
-				SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/pt-pt-scenarios.zip"
+				SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/$RELEASE/pt-pt-scenarios.zip"
 			elif [ "$LANG" = "pt-br" ]; then
-				SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/pt-br-scenarios.zip"
+				SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/$RELEASE/pt-br-scenarios.zip"
 			else
-				SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/pt-pt-scenarios.zip"
+				SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/$RELEASE/pt-pt-scenarios.zip"
 			fi
 		fi
 		if [ "$MAIN_LANG_PREFIX" = "zh" ]; then
 			if [ "$LANG" = "zh-cn" ]; then
-				SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/zh-cn-scenarios.zip"
+				SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/$RELEASE/zh-cn-scenarios.zip"
 			elif [ "$LANG" = "zh-tw" ]; then
-				SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/zh-tw-scenarios.zip"
+				SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/$RELEASE/zh-tw-scenarios.zip"
 			else
-				SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/zh-cn-scenarios.zip"
+				SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/$RELEASE/zh-cn-scenarios.zip"
 			fi
 		fi
 		if [ "$MAIN_LANG_PREFIX" = "cs" ]; then
-			SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/$MAIN_LANG_PREFIX-cz-scenarios.zip"
+			SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/$RELEASE/$MAIN_LANG_PREFIX-cz-scenarios.zip"
 		fi
 		if [ "$MAIN_LANG_PREFIX" = "ja" ]; then
-			SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/$MAIN_LANG_PREFIX-jp-scenarios.zip"
+			SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/$RELEASE/$MAIN_LANG_PREFIX-jp-scenarios.zip"
 		fi
 		if [ "$MAIN_LANG_PREFIX" = "ko" ]; then
-			SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/$MAIN_LANG_PREFIX-kr-scenarios.zip"
+			SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/$RELEASE/$MAIN_LANG_PREFIX-kr-scenarios.zip"
 		fi
 		if [ "$MAIN_LANG_PREFIX" = "sv" ]; then
-			SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/v1.0.1/$MAIN_LANG_PREFIX-se-scenarios.zip"
+			SCENARIOS="https://github.com/MicrosoftDocs/executable-docs/releases/download/$RELEASE/$MAIN_LANG_PREFIX-se-scenarios.zip"
 		fi
 	fi
 fi
